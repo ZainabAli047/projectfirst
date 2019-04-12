828f0b7bce285e2b7887a3e10927989e16b4a536
zainab <zainabali047@gmail.com>
2019-04-12 09:19:37 +0530

    profile

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -1,5 +1,21 @@
 class WelcomesController < ApplicationController
   before_action :authenticate_user! 
 
+  def edit
+    @profile = current_user.profile
+  end
 
+  def update
+    debugger
+    if current_user.profile.update(profile_params)
+      redirect_to root_path
+    else
+      redirect_to edit_welcome_path(current_user.profile)
+    end
+  end
+
+  private
+  def profile_params
+    params.require(:profile).permit(:first_name, :last_name, :age, :about, :email, :gender, hobbies:[])    
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_05_132952) do
+ActiveRecord::Schema.define(version: 2019_04_12_032918) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "profiles", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.integer "age"
+    t.string "gender"
+    t.string "about"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "hobbies", default: [], array: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -21,6 +34,18 @@ ActiveRecord::Schema.define(version: 2019_04_05_132952) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
+    t.datetime "locked_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
